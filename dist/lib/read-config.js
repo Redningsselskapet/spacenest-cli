717eaddc505838592a5e1550fe8795c10c048f27
Bernt Anker <bernt.anker@rs.no>
2021-05-10 00:15:53 +0200

    fixed deployment

--- dist/lib/read-config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.readSpaceNestConfig = void 0;
-var fs_1 = require("fs");
-var readSpaceNestConfig = function (path) {
-    return JSON.parse(fs_1.readFileSync(path, "utf-8"));
-};
-exports.readSpaceNestConfig = readSpaceNestConfig;
